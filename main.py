60f286cab33beb4b29ce7c4bedaadce7d18964c8
mandrej <milan.andrejevic@gmail.com>
2013-05-03 13:27:08 +0200

    paginator, search parameters

--- main.py
+++ main.py
@@ -6,18 +6,16 @@ import webapp2
 import jinja2
 import time
 import tweepy
+import urllib
 import logging
+import datetime
+from tweepy.utils import unescape_html
 from google.appengine.api import users
 from webapp2 import WSGIApplication
 from webapp2_extras.jinja2 import get_jinja2
 from webapp2_extras.appengine.users import login_required
 from models import UserCredentials
 
-TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
-ENV = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
-    autoescape=True
-)
 CONSUMER_KEY = '6GuIfrWPKuAp7UDMT17GA'
 CONSUMER_SECRET = '6IqWHpS3MkU2XsnIzehvfctTHnqEs3hOPWFznijRzG4'
 if os.environ.get('SERVER_SOFTWARE', '').startswith('Dev'):
@@ -25,6 +23,44 @@ if os.environ.get('SERVER_SOFTWARE', '').startswith('Dev'):
 else:
     CALLBACK = 'http://gatwitbot.appspot.com/oauth/callback'
 
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
+ENV = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
+    autoescape=True
+)
+
+
+def quote(s):
+    return urllib.quote_plus(s)
+
+
+def unquote(s):
+    return urllib.unquote_plus(s)
+
+
+# https://twitter.com/search?q=%23energyideas&src=hash
+def timesince_jinja(value, default="just now"):
+    now = datetime.datetime.utcnow()
+    diff = now - value
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+    for period, singular, plural in periods:
+        if period:
+            return "%d %s ago" % (period, singular if period == 1 else plural)
+    return default
+
+ENV.filters.update({
+    'quote': quote,
+    'unquote': unquote,
+    'timesince': timesince_jinja,
+})
 
 class BaseHandler(webapp2.RequestHandler):
     @webapp2.cached_property
@@ -104,8 +140,12 @@ class Index(BaseHandler):
         auth.set_request_token(credentials.token_key, credentials.token_secret)
         auth.set_access_token(credentials.access_key, credentials.access_secret)
 
+        page = int(self.request.get('page', 1))
+        query = unquote(self.request.get('q', 'night'))
+        geocode = unquote(self.request.get('geocode', '44.833,20.463,20km'))
+
         auth_api = tweepy.API(auth)
-        collection = auth_api.home_timeline(count=10)
+        collection = auth_api.search(q=query, geocode=geocode, rpp=10, page=page, count=20)
 
         self.render_template('index.html', {
             'collection': collection,
